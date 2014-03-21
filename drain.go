9907c8f85248c7c5ebc8d20e30d00eb6f589c7da
Pedro Belo <pedro@heroku.com>
2014-03-21 14:42:37 -0700

    hrr

---
+++ drain.go
@@ -0,0 +1,79 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"strings"
+	"net/http"
+	"regexp"
+	"github.com/garyburd/redigo/redis"
+)
+
+func DrainHandler(redis redis.Conn, w http.ResponseWriter, r *http.Request) {
+	defer r.Body.Close()
+	bodyInBytes, _ := ioutil.ReadAll(r.Body)
+	body := string(bodyInBytes)
+	id, secret := ParsePath(r.URL.Path)
+	DrainStore(redis, id, secret, body)
+	fmt.Fprintf(w, "")
+}
+
+func DrainStore(r redis.Conn, id string, secret string, body string) bool {
+	// STABLE AS FUCK
+	if (!strings.Contains(body, "host heroku router - at=info method=")) {
+		return false
+	}
+
+	key := fmt.Sprintf("auth-%s", id)
+	existing, _ := redis.String(r.Do("GET", key))
+
+	if (existing != secret) {
+		return false
+	}
+
+	matchMethod, _ := regexp.Compile(`method=(\w+)`)
+	method := matchMethod.FindStringSubmatch(body)
+
+	matchPath, _ := regexp.Compile(`path=([^ ]+)`)
+	path := matchPath.FindStringSubmatch(body)
+
+	matchStatus, _ := regexp.Compile(`status=(\d+)`)
+	status := matchStatus.FindStringSubmatch(body)
+	return true
+}
+
+func ParsePath(path string) (string, string) {
+	parts := strings.Split(path, "/")
+	if (len(parts) != 4) {
+		return "", ""
+	}
+	return parts[2], parts[3]
+}
+
+func InitRedis() redis.Conn {
+	redis, err := redis.Dial("tcp", ":6379")
+	if err != nil {
+		panic(err)
+	}
+	return redis
+}
+
+func main() {
+	redis := InitRedis()
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		DrainHandler(redis, w, r)
+	})
+
+	port := "8000"
+	if os.Getenv("PORT") != "" {
+		port = os.Getenv("PORT")
+	}
+	fmt.Println("listening on " + port)
+
+    err := http.ListenAndServe(":" + port, nil)
+    if err != nil {
+      panic(err)
+    }
+}
---
+++ drain_test.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+  "testing"
+)
+
+func Test_ParsePath_Error(t *testing.T) {
+  id, secret := ParsePath("/wrong")
+  if (id != "") {
+    t.Error("Expected /wrong to return empty id, got", id)
+  }
+  if (secret != "") {
+    t.Error("Expected /wrong to return empty secret, got", secret)
+  }
+}
+
+func Test_ParsePath_Ok(t *testing.T) {
+  id, secret := ParsePath("/drains/foo/bar")
+  if (id != "foo") {
+    t.Error("Expected /drains/foo/bar to return id foo, got", id)
+  }
+  if (secret != "bar") {
+    t.Error("Expected /drains/foo/bar to return secret bar, got", secret)
+  }
+}
+
+func Test_DrainStore_BadLog(t *testing.T) {
+  r := InitRedis()
+  r.Do("SET", "auth-1", "abc")
+  if (DrainStore(r, "1", "abc", "bad")) {
+    t.Error("Expected it to ignore log")
+  }
+}
+
+func Test_DrainStore_BadId(t *testing.T) {
+  r := InitRedis()
+  r.Do("DEL", "auth-1")
+  log := "241 <158>1 2014-02-25T08:42:07.784181+00:00 host heroku router - at=info method=GET path=/foo host=pedro-dev.herokuapp.com request_id=ccdec783-b755-4b25-802e-00b1f99ee357 fwd=\"199.21.84.17\" dyno=web.1 connect=2ms service=22ms status=200 bytes=5077"
+  if (DrainStore(r, "1", "abc", log)) {
+    t.Error("Expected it to ignore log")
+  }
+}
+
+func TestDrainStore_Works(t *testing.T) {
+  r := InitRedis()
+  r.Do("SET", "auth-1", "abc")
+  log := "241 <158>1 2014-02-25T08:42:07.784181+00:00 host heroku router - at=info method=GET path=/foo host=pedro-dev.herokuapp.com request_id=ccdec783-b755-4b25-802e-00b1f99ee357 fwd=\"199.21.84.17\" dyno=web.1 connect=2ms service=22ms status=200 bytes=5077"
+  if (!DrainStore(r, "1", "abc", log)) {
+    t.Error("Expected it to return true")
+  }
+}
